a8fec64cb1a4f1d2ceab4912e64d5edbcef187d0
Jerome PREVOTAT <jprevota@e1r4p23.42.fr>
2017-10-10 14:31:24 +0200

    added new gnl and new ft_strsplit

--- libft/get_next_line.c
+++ libft/get_next_line.c
@@ -3,95 +3,16 @@
 /*                                                        :::      ::::::::   */
 /*   get_next_line.c                                    :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: jprevota <marvin@42.fr>                    +#+  +:+       +#+        */
+/*   By: jprevota <jprevota@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2017/03/16 14:59:53 by jprevota          #+#    #+#             */
-/*   Updated: 2017/05/20 23:36:00 by admin            ###   ########.fr       */
+/*   Updated: 2017/10/10 14:26:30 by jprevota         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "./get_next_line.h"
-#include "./libft.h"
-#include <sys/types.h>
-#include <sys/stat.h>
-#include <fcntl.h>
 
-int		get_next_line(const int fd, char **line)
-{
-	static char	*buff_end;
-	char			*buff;
-	int			i;
-	int			ret;
-
-	if (!line || !(*line = (char *)malloc(BUFF_SIZE + 1 * sizeof(char)))
-		|| fd < 0)
-		return (-1);
-	ft_memset(*line, '\0', (size_t)(BUFF_SIZE + 1));
-	if (buff_end == NULL)
-	{
-		if (!(buff_end = (char *)malloc((BUFF_SIZE + 1) * sizeof(char))))
-			return (-1);
-		ft_memset(buff_end, '\0', (size_t)(BUFF_SIZE + 1));
-	}
-	if (!(buff = (char *)malloc((BUFF_SIZE + 1) * sizeof(char))))
-		return (-1);
-	ft_memset(buff, '\0', (size_t)(BUFF_SIZE + 1));
-	i = 0;
-	ret = 1;
-	buff = str_memcat(buff, buff_end, ft_strlen(buff_end));
-	ft_memset(buff_end, '\0', ft_strlen(buff_end));
-	while (ret > 0)
-	{
-		if (check_nl(buff) == 1)
-		{
-			while (buff[i] != '\n')
-				i++;
-			*line = str_memcat(*line, buff, i);
-			buff_end = ft_strchr(buff, '\n') + 1;
-			return (1);
-		}
-		*line = str_memcat(*line, buff, ft_strlen(buff));
-		if ((ret = fill_buffer(fd, buff)) == -1)
-			return (-1);
-		if (ret == 0 && ft_strlen(*line) > 0)
-			return (1);
-	}
-	return (0);
-}
-
-int 	fill_buffer(int fd, char *buff)
-{
-	int ret;
-	char *buff2;
-
-	if (!(buff2 = (char *)malloc(BUFF_SIZE + 1 * sizeof(char))))
-		return (-1);
-	ft_memset(buff2, '\0', (size_t)(BUFF_SIZE + 1));
-	if ((ret = read(fd, buff2, BUFF_SIZE)) == -1)
-		return (-1);
-	buff2[ret] = '\0';
-	buff = ft_memmove(buff, buff2, BUFF_SIZE);
-	if (buff2 != NULL)
-		free(buff2);
-	return (ret);
-}
-
-char 	*str_memcat(char *mem1, char *mem2, size_t size)
-{
-	char *tmp;
-
-	if (!(tmp = (char *)malloc(ft_strlen(mem1) + size + 1)))
-		return (NULL);
-	ft_memset(tmp, '\0', (size_t)(ft_strlen(mem1) + size + 1));
-	ft_memcpy(tmp, mem1, ft_strlen(mem1));
-	ft_memcpy(tmp + ft_strlen(mem1), mem2, size);
-	tmp[ft_strlen(mem1) + size] = '\0';
-	if (mem1 != NULL)
-		free(mem1);
-	return (tmp);
-}
-
-int		check_nl(char *str)
+static int	check_nl(char *str)
 {
 	size_t	i;
 
@@ -99,8 +20,73 @@ int		check_nl(char *str)
 	while (i < ft_strlen(str))
 	{
 		if (str[i] == '\n')
-			return (1);
+			return (i);
 		i++;
 	}
+	return (-1);
+}
+
+static char	*str_memcat(char *mem1, char *mem2, size_t size, int reset)
+{
+	char	*tmp;
+
+	if (!(tmp = (char *)malloc(ft_strlen(mem1) + size + 1)))
+		return (NULL);
+	ft_memset(tmp, '\0', (size_t)(ft_strlen(mem1) + size + 1));
+	ft_memcpy(tmp, mem1, ft_strlen(mem1));
+	ft_memcpy(tmp + ft_strlen(mem1), mem2, size);
+	tmp[ft_strlen(mem1) + size] = '\0';
+	if (reset == 1)
+		ft_memset(mem2, '\0', 256);
+	if (mem1 != NULL)
+		free(mem1);
+	return (tmp);
+}
+
+static int	fill_buffer(int fd, char *buff)
+{
+	int		ret;
+
+	if ((ret = read(fd, buff, BUFF_SIZE)) == -1)
+		return (-1);
+	buff[ret] = '\0';
+	return (ret);
+}
+
+static int	gnl(char **line, const int fd, int ret)
+{
+	static char	buff_end[256];
+	int			i;
+
+	while (ret > 0)
+	{
+		if ((i = check_nl(buff_end)) >= 0)
+		{
+			*line = str_memcat(*line, buff_end, i, 0);
+			ft_strcpy(buff_end, buff_end + i + 1);
+			return (1);
+		}
+		if (ft_strlen(buff_end) + BUFF_SIZE + 1 > 256)
+			*line = str_memcat(*line, buff_end, ft_strlen(buff_end), 1);
+		if ((ret = fill_buffer(fd, buff_end + ft_strlen(buff_end))) == -1)
+			return (-1);
+		if (ret == 0 && ft_strlen(buff_end) > 0)
+		{
+			*line = str_memcat(*line, buff_end, ft_strlen(buff_end), 1);
+			return (1);
+		}
+	}
 	return (0);
+}
+
+int			get_next_line(const int fd, char **line)
+{
+	int		ret;
+
+	ret = 1;
+	if (!line || !(*line = (char *)malloc(BUFF_SIZE + 1 * sizeof(char)))
+		|| fd < 0)
+		return (-1);
+	ft_memset(*line, '\0', (size_t)(BUFF_SIZE + 1));
+	return (gnl(line, fd, ret));
 }
--- libft/get_next_line.h
+++ libft/get_next_line.h
@@ -6,19 +6,19 @@
 /*   By: jprevota <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2016/11/28 16:44:38 by jprevota          #+#    #+#             */
-/*   Updated: 2017/04/20 15:56:30 by admin            ###   ########.fr       */
+/*   Updated: 2017/10/10 14:23:33 by jprevota         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #ifndef GET_NEXT_LINE_H
 # define GET_NEXT_LINE_H
 
-# define BUFF_SIZE 5
-# include "./libft.h"
+# define BUFF_SIZE 10
+# include "./libft/libft.h"
+# include <sys/types.h>
+# include <sys/stat.h>
+# include <fcntl.h>
 
 int		get_next_line(const int fd, char **line);
-int		check_nl(char *str);
-int     fill_buffer(int fd, char *buff_end);
-char    *str_memcat(char *mem1, char *mem2, size_t size);
 
 #endif
